3f543fc2c9848b2632fca6e176d69523c9e9917b
Sam <sam.eskandar00@hotmail.com>
2020-05-14 19:53:12 -0400

    format generic views

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import TodosList from "./Views/TodosList";
+import EditTodo from "./Views/EditTodo";
+import CreateTodo from "./Views/CreateTodo";
+import Navbar from "./Components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={TodosList} />
+                    <Route exact path="/edit/:id" component={EditTodo}/>
+                    <Route exact path="/create" component={CreateTodo}/>
+                </Switch>
+            </Router>
+        </div>
+    );
+  }
 }
 
 export default App;

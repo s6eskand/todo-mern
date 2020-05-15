90fb3ac717a11d725395d9547e39e951f7acfbb7
Sam <sam.eskandar00@hotmail.com>
2020-05-14 20:28:44 -0400

    update 'create' styles

--- src/Views/CreateTodo.js
+++ src/Views/CreateTodo.js
@@ -1,10 +1,72 @@
 import React from 'react';
 
 class CreateTodo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleResponsibleChange = this.handleResponsibleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            description: '',
+            responsible: '',
+            priority: '',
+            isComplete: false,
+        }
+    }
+
+    handleDescriptionChange = (e) => {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    handleResponsibleChange = (e) => {
+        this.setState({
+            responsible: e.target.value
+        })
+    }
+
+    handlePriorityChange = (e) => {
+        this.setState({
+            priority: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Form Submitted')
+        console.log(this.state)
+
+        this.setState({
+            description: '',
+            responsible: '',
+            priority: '',
+            isComplete: false,
+        })
+    }
+
+
     render() {
         return(
-            <div className="container">
-                <p>Create a Todo Here!</p>
+            <div style={{paddingTop: '100'}}>
+                <div className="container">
+                    <h3>Create a Todo Here!</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label>Description: </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={this.state.description}
+                                onChange={this.handleDescriptionChange}
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }

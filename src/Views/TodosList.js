ce8d77a4a16f1bd9871f3a59ae7cd902d1d4112a
Sam <sam.eskandar00@hotmail.com>
2020-05-15 19:01:05 -0400

    update View with data from database

--- src/Views/TodosList.js
+++ src/Views/TodosList.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Todo from "../Components/Todo";
 
 class TodosList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos')
+            .then(res => {
+                this.setState({
+                    todos: res.data
+                })
+            })
+            .catch(err => console.error(err));
+    };
+
+    todoList = () => {
+        if (this.state) {
+            return (
+                this.state.todos.map((cur, i) => {
+                    return <Todo todo={cur} key={i}/>
+                })
+            )
+        }
+    }
+
     render() {
         return(
             <div className="todo-component">
                 <div className="container">
-                    <p>The Full List of Todos!</p>
+                    <h3>The Full List of Todos!</h3>
+                    <table className="table table-striped todo-component">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Responsible</th>
+                                <th>Priority</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { this.todoList() }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
